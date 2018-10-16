7c041ab9b8f5a2a8d9518a463a646a10b237b3f1
JISHAN GARGACHARYA <34843832+jishangarg@users.noreply.github.com>
2018-10-17 01:13:47 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,1 +1,22 @@
 import java.util.*;
+public class Main{
+  
+  public static int fib(int n,int[] arr)
+  {
+    if(n==0 || n==1)
+      return 1;
+    if(arr[n]!=0)
+      return arr[n];
+    int output=fib(n-1,arr)+fib(n-2,arr);
+    arr[n]=output;
+    return output;
+  }
+  
+  public static void main(String[]args)
+  {
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int[] arr=new int[n+1];
+    System.out.println(fib(n,arr));
+  }
+}
